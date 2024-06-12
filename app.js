56165a6d22555a021651df7597ab1b60f3c12431
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-12 20:55:30 +0300

    chore: Update dependencies and add mongoose package for database integration

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//CORS handling
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 //Routes which should handle requests
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
