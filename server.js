7a26ead986627e022d0f871b5462d7714d322f70
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-07-26 19:56:10 +0300

    feat: Add product routes for GET and POST requests

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const http = require('http');
-
+const express = require('express');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
