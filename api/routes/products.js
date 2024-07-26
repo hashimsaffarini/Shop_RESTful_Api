94a8cc8042412aecd246172ddca297bd39100c76
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-07-26 20:10:10 +0300

    refactor: Add GET, PATCH, and DELETE routes for products

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,4 +14,31 @@ router.post('/', (req, res, next) => {
   });
 });
 
+router.get('/:productId', (req, res, next) => {
+  const { productId } = req.params;
+
+  if (productId === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special ID',
+      id: productId
+    });
+  } else {
+    res.status(200).json({
+      message: 'You passed an ID'
+    });
+  }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated product!'
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted product!'
+  });
+});
+
 module.exports = router;
