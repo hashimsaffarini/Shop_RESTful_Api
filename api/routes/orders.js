b39b1fe6f820d9ea0196639b7ff643c6a7f11685
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-07-26 20:25:48 +0300

    chore: Add order routes for GET, POST, and DELETE requests

---
+++ api/routes/orders.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders were fetched'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Order was created'
+  });
+});
+
+router.get('/:orderId', (req, res, next) => {
+
+  const { orderId } = req.params;
+  res.status(200).json({
+    message: 'Order details',
+    orderId: orderId
+  });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+
+  const { orderId } = req.params;
+  res.status(200).json({
+    message: 'Order deleted',
+    orderId: orderId
+  });
+});
+
+module.exports = router;
