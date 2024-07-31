4f8f796b168157faa872a9ab75e0ba0568e64ea3
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-07-31 14:35:52 +0300

    chore: Add order and product routes for creating and retrieving data

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const orderRoutes = require('./api/routes/orders');
 //for logging the requests in the console
 app.use(morgan('dev'));
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
